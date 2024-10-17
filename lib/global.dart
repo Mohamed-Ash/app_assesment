0a23978b767e7dab77f98a624c408a1988031f05
Mohamed-Ash <87867424+Mohamed-Ash@users.noreply.github.com>
2024-10-17 04:24:22 +0300

    add name

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+const String name = 'App Assesment';
+
 late final GlobalKey<NavigatorState> navigatorKey;
+
 late final GoRouter appRouter;
