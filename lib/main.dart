0e97db5e966b0c919a37e3b8272de21872a1bbd4
abdelrahman <86198822+Abdelrahmann1@users.noreply.github.com>
2024-10-19 15:07:56 +0200

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:app_assesment/app.dart';
 import 'package:app_assesment/app_router.dart';
 import 'package:app_assesment/core/service/hive_service.dart';
+import 'package:app_assesment/firebase_options.dart';
 import 'package:app_assesment/global.dart';
 import 'package:app_assesment/screens/home/page/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
   debugPrint('[app_assesment] Starting App');
+    WidgetsFlutterBinding.ensureInitialized();
 
-    
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   debugPrint('[app_assesment] Initializing Services');
   await HiveService().initialize();
-    
   navigatorKey = GlobalKey<NavigatorState>();
   appRouter = GoRouter(
     initialLocation: HomePage.pageRoute.path,
@@ -21,8 +25,6 @@ void main() async {
     debugLogDiagnostics: true,
   );
 
-
-  
   runApp(const App());
   debugPrint('[app_assesment]  App Started');
 }
